1adeb18401b2ef7c9e15d0a362917f8091d127c9
Rafael Mateo <76257337+rafaelmateoh@users.noreply.github.com>
2020-12-25 20:24:18 -0500

    Update ex6.c

--- C/Soluciones Nivel 1/ex6.c
+++ C/Soluciones Nivel 1/ex6.c
@@ -5,34 +5,19 @@
  */
 
 #include <stdio.h>
-#include <stdlib.h>
-
-int factorial();
+#include <stdio.h>
 
 int main() {
-    int a,b;
-    printf("Enter a number: ");
-    scanf("%i",&a);
-    printf("Enter a number: ");
-    scanf("%i",&b);
+    int n,i;
+    int function = 1;
     
-    factorial(a,b);
-    
+    printf("Enter a number: ");
+    scanf("%d",&n);
+    for(i = 1; i <= n; i++){ //
+        function *= i;
+    }
+    printf("The result is %d \n", function);
     return 0;
 }
 
-int factorial(int a, int b){
-    int x,result1 = 0,result2 = 0;
-    
-    for(x = 0; x <= a; x++){
-        result1 = x * a;
-    }
-    printf("The result is %d \n",result1);
-    
-    for(x = 0; x <= b; x++){
-        result2 = x * b;
-    }
-    printf("The result is %d \n",result2);
-    
-    return 0;
-}
+
